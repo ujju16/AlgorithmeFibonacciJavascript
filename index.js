1a601f91c4911f4757962e520caa2306f733020d
Julien PERRIN <jul.darrouza@gmail.com>
2022-08-31 23:40:47 +0000

    Création de la boucle for

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const n = Number(prompt('Entrer un nombre entier supérieur à 1: '))
 var fibo = new Array(n+1)
 fibo[0] = 0
 fibo[1] = 1
+
+for (let i = 2; i < n+1; i++) {
+  fibo[i] = fibo[i-1] + fibo[i-2]
+}
